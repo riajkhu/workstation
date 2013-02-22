246165273a26f757d4933a49d89ecc63588bcd37
Yun Mao <yunmao@gmail.com>
2013-02-22 14:50:59 -0500

    Automatic create a eth#{bridge_name} nic
    This nic is uplinked to the vxlan ovs bridge and is persistent
    across reboot

--- recipes/network-vxlan.rb
+++ recipes/network-vxlan.rb
@@ -1,9 +1,9 @@
 # setup a vxlan network between all nodes in the environment
 # in a hub and spoke topology
 
-bridge_name = "br0"
+# must specify node["openvswitch"]["hub_name"] and node["openvswitch"]["vxlan_bridges"]
 hub = node["openvswitch"]["hub_name"]
-remote_nodes = [] 
+remote_nodes = []
 if node.name == hub then
   # I'm the hub, I build a tunnel with everybody else
   query = "chef_environment:#{node.chef_environment}"
@@ -22,6 +22,40 @@ end
 
 remote_ips = remote_nodes.map{|n| n["ipaddress"]}
 
-node.default["openvswitch"]["vxlan"][bridge_name] = remote_ips
+node["openvswitch"]["vxlan_bridges"].each do |bridge_name|
+  node.default["openvswitch"]["vxlan"][bridge_name] = remote_ips
+end
 
 include_recipe "openvswitch::vxlan"
+
+node["openvswitch"]["vxlan_bridges"].each do |bridge_name|
+  eth_name = "eth#{bridge_name}"
+  eth_peer_name = "#{eth_name}in" # can't be too long
+  template "/etc/network/interface.#{eth_name}" do
+    source "interface.erb"
+    owner "root"
+    group "root"
+    mode 0644
+    variables(
+              :bridge_name => bridge_name,
+              :eth_name => eth_name,
+              :eth_peer_name => eth_peer_name
+              )
+  end
+  bash "install and start interface" do
+    code <<-EOH
+      cat /etc/network/interface.#{eth_name} >> /etc/network/interfaces
+    EOH
+    not_if "grep #{eth_name} /etc/network/interfaces"
+  end
+  
+  bash "make sure the inside port is in ovs" do
+    code <<-EOH
+      ifup #{eth_name} || true
+      # ovs will remember this across reboot
+      ovs-vsctl --may-exist add-port #{bridge_name} #{eth_peer_name}
+    EOH
+  end
+
+end
+
