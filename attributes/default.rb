8c48996079f195117bb2f2f1cdcdab06019fb5df
sean <sean@torandu.com>
2013-04-23 15:59:30 -0700

    Fix FC019: Access node attributes in a consistent manner

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
-default['openvswitch']['install_path'] = '/opt/ovs'
-default['openvswitch']['code_version'] =  \
+default[:openvswitch][:install_path] = '/opt/ovs'
+default[:openvswitch][:code_version] =  \
    '3b6f2889400fd340b851c2d36356457559ae6e81' # bug-free VXLAN impl
